86c2d8b7284e4ca511ebdbce1237f2872b7d9312
mullinm1 <132863105+mullinm1@users.noreply.github.com>
2025-06-22 20:20:09 -0400

    Create CapabilityAssessmentTool.jsx

---
+++ src/components/CapabilityAssessmentTool.jsx
@@ -0,0 +1,15 @@
+// Placeholder for CapabilityAssessmentTool component
+// Will be replaced with actual component code
+
+import React from 'react';
+
+function CapabilityAssessmentTool() {
+  return (
+    <div>
+      <h1>Military Capability Assessment Tool</h1>
+      <p>Component placeholder - will be updated with full tool</p>
+    </div>
+  );
+}
+
+export default CapabilityAssessmentTool;
