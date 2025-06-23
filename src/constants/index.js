d6a7699c546b41bdae4dec4927013d6c0818a7ec
mullinm1 <132863105+mullinm1@users.noreply.github.com>
2025-06-22 20:21:43 -0400

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+// Placeholder for all assessment data constants
+// Will contain REGIONAL_BUREAUS, PARTNER_TYPES, threats, etc.
+
+export const REGIONAL_BUREAUS = {
+  // Data will be added here
+};
+
+export const PARTNER_TYPES = [
+  // Data will be added here
+];
+
+// More constants will be added here
