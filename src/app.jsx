0cdde416768053545467842a3ac1f7bf7d50bf56
mullinm1 <132863105+mullinm1@users.noreply.github.com>
2025-06-22 20:24:30 -0400

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CapabilityAssessmentTool from './components/CapabilityAssessmentTool';
+
+function App() {
+  return (
+    <div className="App">
+      <CapabilityAssessmentTool />
+    </div>
+  );
+}
+
+export default App;
